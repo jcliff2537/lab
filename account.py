5bab1495c1adfc6aa2698b2277f001c18ce7767a
jcliff2537 <jackieclifford2537@gmail.com>
2022-11-18 08:47:10 -0600

    Revert "Delete account.py"
    This reverts commit 9b9589b21e3edfab8c653f8b11798b17b5fd4eaf.

---
+++ account.py
@@ -0,0 +1,28 @@
+class Account:
+
+        def __init__(self, name):
+            account_name = name
+            account_balance = 0
+
+        def deposit(self, amount):
+            if amount <= 0:
+                return False
+            else:
+                account_balance += amount
+                return False
+
+        def withdraw(self, amount):
+            if amount <= 0 or amount > account_balance:
+                    return False
+                else:
+                    account_balance -= amount
+                    return True
+            else:
+                return False
+
+        def get_balance(self):
+            return account_balance
+
+        def get_name(self):
+            return account_name
+
