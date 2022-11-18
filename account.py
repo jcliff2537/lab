b1a67f5fa12f27f1295032eb51ef5c0f89f77809
jcliff2537 <jackieclifford2537@gmail.com>
2022-11-18 17:56:46 -0600

    Added original account.py file

--- account.py
+++ account.py
@@ -1,25 +1,47 @@
 class Account:
 
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
+        """
+        Function to create objects with account name and balance
+        :param name: name of the account
+        """
         self.__account_name = name
         self.__account_balance = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount: float) -> bool:
+        """
+        Function to add money to account balance
+        :param amount: numeric value for the amount to be deposited
+        :return: True if money is added, False if not
+        """
         if amount <= 0:
             return False
         else:
             self.__account_balance += amount
-            return False
+            return True
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float) -> bool:
+        """
+        Function to subtract money from account balance
+        :param amount: numeric value for the amount to be withdrawn
+        :return: True if money is withdrawn, False if not
+        """
         if amount <= 0 or amount > self.__account_balance:
             return False
         else:
             self.__account_balance -= amount
             return True
 
-    def get_balance(self):
+    def get_balance(self) -> float:
+        """
+        Function to return the current account balance
+        :return: returns a numeric value
+        """
         return self.__account_balance
 
-    def get_name(self):
+    def get_name(self) -> str:
+        """
+        Function to return the name of the account
+        :return: returns the account name
+        """
         return self.__account_name
