c9c4675cef3f848f67d5efe9153d056f57d8a8ee
jcliff2537 <jackieclifford2537@gmail.com>
2022-11-18 18:06:36 -0600

    Added original account.py file

--- test_account.py
+++ test_account.py
@@ -17,36 +17,32 @@ class Test:
 
     def test_deposit(self):
         assert self.p1.deposit(-10) is False
-        assert self.p1.get_balance == 0
+        assert self.p1.get_balance() == 0
 
         assert self.p1.deposit(0) is False
-        assert self.p1.get_balance == 0
+        assert self.p1.get_balance() == 0
 
         assert self.p1.deposit(10) is True
-        assert self.p1.get_balance == 10
+        assert self.p1.get_balance() == 10
 
         assert self.p1.deposit(20.5) is True
-        assert self.p1.get_balance == approx(30.5,abs=.001)
+        assert self.p1.get_balance() == approx(30.5,abs=.001)
 
-        #assert self.p1.deposit(-10) is False
-        #assert self.p1.get_balance == approx(30.5,abs=.001)
+        assert self.p1.deposit(-10) is False
+        assert self.p1.get_balance() == approx(30.5,abs=.001)
 
     def test_withdraw(self):
         assert self.p1.withdraw(10) is False
-        assert self.p1.get_balance == 0
+        assert self.p1.get_balance() == 0
 
         assert self.p1.withdraw(0) is False
-        assert self.p1.get_balance == 0
+        assert self.p1.get_balance() == 0
 
         self.p1.deposit(50)
 
         assert self.p1.withdraw(10.5) is True
-        assert self.p1.get_balance == approx(39.5,abs=.001)
+        assert self.p1.get_balance() == approx(39.5,abs=.001)
 
         assert self.p1.withdraw(-10) is False
-        assert self.p1.get_balance == approx(39.5,abs=.001)
+        assert self.p1.get_balance() == approx(39.5,abs=.001)
 
-    """
-     make a deposit calling the function self.p1.deposit(30) then
-     make a witdrawal (10.5)
-    """
